df6501a9d2045deba0a9d645a4caaae9c594ec21
HvGaur <hvgaurr@gmail.com>
2024-07-26 19:50:24 +0530

    added validators for input

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -6,7 +6,7 @@ import "./NewPlace.css";
 const NewPlace = () => {
     return (
         <form className="place-form">
-            <Input type="text" label = "Title" />
+            <Input element = "input" type="text" label = "Title" validators={[]} errorText = "Invalid Title"/>
         </form>
     );
 };
