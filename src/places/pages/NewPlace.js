33d53c9ef788ba4f2227c304dc230090fc8bfa43
HvGaur <hvgaurr@gmail.com>
2024-07-26 19:59:49 +0530

    input validation

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -1,14 +1,21 @@
-import React from "react";
-import Input from "../../shared/components/FormElements/Input";
+import React from 'react';
 
-import "./NewPlace.css";
+import Input from '../../shared/components/FormElements/Input';
+import { VALIDATOR_REQUIRE } from '../../shared/util/validators';
+import './NewPlace.css';
 
 const NewPlace = () => {
-    return (
-        <form className="place-form">
-            <Input element = "input" type="text" label = "Title" validators={[]} errorText = "Invalid Title"/>
-        </form>
-    );
+  return (
+    <form className="place-form">
+      <Input
+        element="input"
+        type="text"
+        label="Title"
+        validators={[VALIDATOR_REQUIRE()]}
+        errorText="Please enter a valid title."
+      />
+    </form>
+  );
 };
 
 export default NewPlace;
